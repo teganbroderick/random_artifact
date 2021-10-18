6f3143a7076a1fe66ea643593d39802d6e07a540
Tegan Broderick <teganbroderick@gmail.com>
2021-10-17 19:16:08 -0700

    Add twilio functionlity to text random artifact information to user

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem 'bootsnap', '>= 1.4.2', require: false
 
 gem 'uri'
 gem 'net-http'
+gem 'twilio-ruby'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-    root "random_artifact#index"
+  root "random_artifact#index"
 
-    get "/random_artifact", to: "random_artifact#index"
+  get "/random_artifact", to: "random_artifact#index"
+
+  resource :messages do
+    collection do
+      post 'reply'
+    end
+  end
 end
