862d987dadb733478e3f4142e9820038bd873314
Tegan Broderick <teganbroderick@gmail.com>
2023-01-24 15:26:31 -0800

    set up basic index page using react

---
+++ app/javascript/components/Home.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default () => (
+  <div>
+    <h1>A Random Artifact 2.0</h1>
+    <p>Work In Progress</p>
+  </div>
+);
