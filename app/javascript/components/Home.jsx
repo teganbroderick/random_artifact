54113ee5fe09af1e24baca60bcbc7b519e8bedb1
Tegan Broderick <teganbroderick@gmail.com>
2023-01-27 12:05:50 -0800

    fetch and display artifact data on the home page

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,9 +1,42 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
-export default () => (
-  <div>
-    <h1>A Random Artifact 2.0</h1>
-    <p>Work In Progress</p>
-  </div>
-);
+const RandomArtifact = () => {
+  const navigate = useNavigate();
+  const [artifact, setArtifact] = useState("");
+  
+  useEffect(() => {
+    const url = `/api/v1/random_artifact/index`
+    fetch(url)
+      .then((response) => {
+        if (response.ok) {
+          console.log("got a response")
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then((response) => {
+        console.log("setting response" + JSON.stringify(response))
+        setArtifact(response)
+
+      })
+      .catch(() => navigate("/"));
+    }, []);
+
+
+  return (
+    <div>
+      <h1>A Random Artifact v2.0</h1>
+
+      <p>Title: {artifact.title}</p>
+      <p>Artist: {artifact.artist_maker}</p>
+      <p>Accession: {artifact.accession_number}</p>
+      <p>Description: {artifact.description}</p>
+      <p>Type: {artifact.object_type}</p>
+      <p>Date: {artifact.date}</p>
+      <img src={artifact.image_url_full} />
+    </div>
+  )
+};
+
+export default RandomArtifact;
