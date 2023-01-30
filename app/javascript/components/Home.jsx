24a8ca6a4094a2cad5f0909e18cd95dff813c279
Tegan Broderick <teganbroderick@gmail.com>
2023-01-30 13:00:57 -0800

    move artifact data into a separate ArtifactData component

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,81 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import ArtifactData from "../components/ArtifactData";
 
 const RandomArtifact = () => {
-  const navigate = useNavigate();
-  const [artifact, setArtifact] = useState("");
-  
-  useEffect(() => {
-    const url = `/api/v1/random_artifact/index`
-    fetch(url)
-      .then((response) => {
-        if (response.ok) {
-          console.log("got a response")
-          return response.json();
-        }
-        throw new Error("Network response was not ok.");
-      })
-      .then((response) => {
-        console.log("setting response" + JSON.stringify(response))
-        setArtifact(response)
-
-      })
-      .catch(() => navigate("/"));
-    }, []);
-
-
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col">
-          <h1>A Random Artifact</h1>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col">
-          <img 
-            src={artifact.image_url_full}
-            width={500}
-          />
-        </div>
-        <div className="col">
-          <table className="table">
-            <tbody>
-              <tr> 
-                <th> Accession Number </th>
-                <td> {artifact.accession_number} </td>
-              </tr>
-              <tr> 
-                <th> Title </th>
-                <td> {artifact.title} </td>
-              </tr>
-              <tr> 
-                <th> Description </th>
-                <td> {artifact.description}</td>
-              </tr>    
-               <tr> 
-                  <th> Object Type </th>
-                  <td> {artifact.object_type} </td>
-              </tr>   
-              <tr> 
-                  <th> Artist/Maker </th>
-                  <td> {artifact.artist_maker}  </td>
-              </tr> 
-              <tr> 
-                <th> Date </th>
-                <td> {artifact.date} </td>
-              </tr>  
-            </tbody>   
-          </table>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col">
-          <p></p>
-        </div>
-      </div>
-    </div>
-  )
+    <ArtifactData/>
+    )
 };
 
 export default RandomArtifact;
